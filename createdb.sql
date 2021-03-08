fe91f7a544c3caa3243338e26060d89759f2e3ca
Fabio Junior Da Costa Amaral <fabio.da_costa_amaral@roche.com>
2021-03-08 09:49:19 +0100

    Init

---
+++ createdb.sql
@@ -0,0 +1,14 @@
+CREATE TABLE stsheader (
+    [id] [int] Identity(1, 1) NOT NULL PRIMARY KEY,
+    [filename] [char(300)] NOT NULL,
+    [feature] [char(300)] NOT NULL,
+    [test_id] [char(300)] NOT NULL,
+    [author] [char(30)] NOT NULL,
+    [reviewer] [char(30)] NOT NULL,
+    [review_date] [char(50)] NOT NULL,
+    [test_type] [char(30)] NOT NULL,
+    [creation_date] [char(50)] NOT NULL,
+    [specifications] [char(5)] NOT NULL,
+    [issues] [char(30)] NOT NULL,
+    [last_changed] [datetime] NOT NULL
+);
